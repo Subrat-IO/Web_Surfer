e3b04650fc68c1176e0364bc3e9e10032db62d36
Subrat Sethi <subratsethi501@gmail.com>
2025-10-02 00:41:42 +0530

    url change

--- Frontend/src/config/axiosInstance.jsx
+++ Frontend/src/config/axiosInstance.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+export const BASE_URL = process.env.REACT_APP_API_URL || "https://web-surfer.onrender.com";
 
-export const BASE_URL = "https://web-surfer.onrender.com"
 export const npmjsserver = axios.create({
   baseURL: BASE_URL,
-  "Content-Type": "application/json",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
